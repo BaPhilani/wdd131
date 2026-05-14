7c1825ea7027edc2e7e2eac11938f69af0b542f5
Victor Mtisi <victormtisi@gmail.com>
2026-05-14 14:22:58 +0200

    main

---
+++ week02/scripts/bom.js
@@ -0,0 +1,3 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('___'); //fill in the blank to reference the unordered list element.
