b364afbeff3c7859a84711344f1fec3bea786581
Victor Mtisi <victormtisi@gmail.com>
2026-05-22 16:54:41 +0200

    main

---
+++ scripts/place.js
@@ -0,0 +1,25 @@
+// Footer Dates
+document.getElementById("year").textContent =
+new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+`Last Modified: ${document.lastModified}`;
+
+// Wind Chill Calculation
+const temperature = 24;
+const windSpeed = 10;
+
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
+    ).toFixed(1);
+}
+
+const windChill =
+calculateWindChill(temperature, windSpeed);
+
+document.getElementById("windchill").textContent = '${windChill}°C';
+windChill + "";
