77c317c39e6aa1bb3e5410b5d4a81e8f078261f4
Victor Mtisi <victormtisi@gmail.com>
2026-06-01 12:03:48 +0200

    main

--- week04/scripts/filtered-temples.js
+++ week04/scripts/filtered-temples.js
@@ -1,80 +1,73 @@
 const temples = [
     {
-        templeName: "Aba Nigeria",
-        location: "Aba, Nigeria",
-        dedicated: "2005, August, 7",
-        area: 11500,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+        templeName: "Accra Ghana",
+        location: "Accra, Ghana",
+        dedicated: "2004, August, 21",
+        area: 9200,
+        imageUrl: "images/accra_ghana_temple.jpeg"
     },
     {
-        templeName: "Manti Utah",
-        location: "Manti, Utah, United States",
-        dedicated: "1888, May, 21",
-        area: 74792,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        templeName: "Adelaide Australia",
+        location: "Adelaide, Australia",
+        dedicated: "2000, April, 13",
+        area: 36200,
+        imageUrl: "images/adelaide_australia_temple.jpeg"
     },
     {
-        templeName: "Payson Utah",
-        location: "Payson, Utah, United States",
-        dedicated: "2015, June, 7",
-        area: 96630,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+        templeName: "Belem Brazil",
+        location: "Belém, Brazil",
+        dedicated: "2000, May, 20",
+        area: 20400,
+        imageUrl: "images/belem_brazil_temple.png"
     },
     {
-        templeName: "Yigo Guam",
-        location: "Yigo, Guam",
-        dedicated: "2020, May, 2",
-        area: 6861,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+        templeName: "Buenos Aires Argentina",
+        location: "Buenos Aires, Argentina",
+        dedicated: "1986, January, 17",
+        area: 55500,
+        imageUrl: "images/buenos_argentina_temple.jpeg"
     },
     {
-        templeName: "Washington D.C.",
-        location: "Kensington, Maryland, United States",
-        dedicated: "1974, November, 19",
-        area: 156558,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+        templeName: "Fukuoka Japan",
+        location: "Fukuoka, Japan",
+        dedicated: "2000, October, 27",
+        area: 19600,
+        imageUrl: "images/fukuoka_japan_temple.jpeg"
     },
     {
-        templeName: "Lima Perú",
-        location: "Lima, Perú",
-        dedicated: "1986, January, 10",
-        area: 9600,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+        templeName: "Harare Zimbabwe",
+        location: "Harare, Zimbabwe",
+        dedicated: "1999, June, 18",
+        area: 7600,
+        imageUrl: "images/harare_zimbabwe_temple.jpg"
     },
     {
-        templeName: "Mexico City Mexico",
-        location: "Mexico City, Mexico",
-        dedicated: "1983, December, 2",
-        area: 116642,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+        templeName: "Johannesburg South Africa",
+        location: "Johannesburg, South Africa",
+        dedicated: "1985, April, 24",
+        area: 18600,
+        imageUrl: "images/johannesburg_temple.jpeg"
     },
     {
-        templeName: "Rome Italy",
-        location: "Rome, Italy",
-        dedicated: "2019, December, 10",
-        area: 58352,
-        imageUrl: "week04/images/placeholder.svg"
+        templeName: "Kyiv Ukraine",
+        location: "Kyiv, Ukraine",
+        dedicated: "2010, August, 29",
+        area: 17500,
+        imageUrl: "images/kyiv_ukraine_temple.jpeg"
     },
     {
-        templeName: "Kirtland Ohio",
-        location: "Kirtland, Ohio, United States",
-        dedicated: "1836, March, 27",
-        area: 9580,
-        imageUrl: "week04/images/placeholder.svg"
+        templeName: "Madrid Spain",
+        location: "Madrid, Spain",
+        dedicated: "1999, July, 7",
+        area: 20300,
+        imageUrl: "images/madrid_spain_temple.jpeg"
     },
     {
-        templeName: "Winter Quarters Nebraska",
-        location: "Florence, Nebraska, United States",
-        dedicated: "2001, April, 22",
-        area: 107900,
-        imageUrl: "week04/images/placeholder.svg"
-    },
-    {
-        templeName: "São Paulo Brazil",
-        location: "São Paulo, Brazil",
-        dedicated: "1978, October, 28",
-        area: 82900,
-        imageUrl: "week04/images/placeholder.svg"
+        templeName: "Salt Lake Temple",
+        location: "Salt Lake City, Utah, United States",
+        dedicated: "1893, April, 6",
+        area: 115000,
+        imageUrl: "images/salt_lake_temple.jpeg"
     }
 ];
 
